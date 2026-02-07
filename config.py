856377e8515cc07524ffadb56004345a9dae2481
mhmdaliraisie <mhmdaliraisie@gmail.com>
2026-02-07 22:47:37 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def must_get(name: str):
     value = os.getenv(name)
@@ -15,17 +12,14 @@ TARGET_CHANNEL_ID = int(must_get("TARGET_CHANNEL_ID"))
 SPONSOR_CHANNEL_ID = int(must_get("SPONSOR_CHANNEL_ID"))
 SPONSOR_CHANNEL_USERNAME = must_get("SPONSOR_CHANNEL_USERNAME")
 
-# ادمین‌ها (آیدی عددی، جدا شده با کاما)
 ADMIN_IDS = [
     int(x.strip())
     for x in os.getenv("ADMIN_IDS", "").split(",")
     if x.strip().isdigit()
 ]
 
-# تگ آخر پیام کانال
 FOOTER_TAG = os.getenv("FOOTER_TAG", "@config2v")
 
-# تایم‌اوت‌ها
 CONNECT_TIMEOUT = int(os.getenv("CONNECT_TIMEOUT", "20"))
 READ_TIMEOUT = int(os.getenv("READ_TIMEOUT", "20"))
 WRITE_TIMEOUT = int(os.getenv("WRITE_TIMEOUT", "20"))
